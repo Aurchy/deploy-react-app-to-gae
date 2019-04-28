e3e196ce6621d6af07e71fea6ac6d8968997099a
Aurchy <farzanahaqueaurchy@gmail.com>
2019-04-28 15:07:27 +0100

    --amend

--- deploy.sh
+++ deploy.sh
@@ -5,18 +5,19 @@ npm run build
 gzip build/static/css/* build/static/js/* build/static/images/*
 for old in build/static/css/*.gz build/static/js/*.gz build/static/images/*.gz; do mv $old ${old%???}; done
 
-# create folder deployable from scratch and move all deployable items to it
-mkdir deployable
-cp app.yaml deployable/
+# create folder deployables from scratch and move all deployable items to it
+mkdir deployables
+cp app.yaml deployables/
 # copy package.json for your node application, not the one for your react app
-cp deploy/package.json deployable/
-cp -r build deployable/
-cp index.js deployable/
+cp deploy/package.json deployables/
+cp -r build deployables/
+# node application entry point
+cp index.js deployables/
 
 # rsync static objects hosted on cloud storage
-gsutil -m rsync -r ./deployable/build/static/css gs://my-staging-bucket/build/static/css
-gsutil -m rsync -r ./deployable/build/static/images gs://my-staging-bucket/build/static/images
-gsutil -m rsync -r ./deployable/build/static/js gs://my-staging-bucket/build/static/js
+gsutil -m rsync -r ./deployables/build/static/css gs://my-staging-bucket/build/static/css
+gsutil -m rsync -r ./deployables/build/static/images gs://my-staging-bucket/build/static/images
+gsutil -m rsync -r ./deployables/build/static/js gs://my-staging-bucket/build/static/js
 
 # set metadata for static objects hosted on cloud storage
 gsutil -m setmeta -h "Content-Encoding: gzip" gs://my-staging-bucket/build/static/css/* \
@@ -26,4 +27,4 @@ gsutil -m setmeta -h "Cache-Control: public, max-age=31536000" gs://my-staging-b
                   gs://my-staging-bucket/build/static/js/* \
                   gs://my-staging-bucket/build/static/images/*
 
-gcloud app deploy deployable/app.yaml -q --version="fe-staging" --no-promote
+gcloud app deploy deployables/app.yaml -q --version="fe-staging" --no-promote
