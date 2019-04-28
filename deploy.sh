85eac979884732a8e414acbb2462f6c735d7ed7a
Aurchy <farzanahaqueaurchy@gmail.com>
2019-04-28 15:05:19 +0100

    IC: Deploy file

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+# will create this two dirs from scratch later
+rm -rf build deployables
+
+npm run build
+gzip build/static/css/* build/static/js/* build/static/images/*
+for old in build/static/css/*.gz build/static/js/*.gz build/static/images/*.gz; do mv $old ${old%???}; done
+
+# create folder deployable from scratch and move all deployable items to it
+mkdir deployable
+cp app.yaml deployable/
+# copy package.json for your node application, not the one for your react app
+cp deploy/package.json deployable/
+cp -r build deployable/
+cp index.js deployable/
+
+# rsync static objects hosted on cloud storage
+gsutil -m rsync -r ./deployable/build/static/css gs://my-staging-bucket/build/static/css
+gsutil -m rsync -r ./deployable/build/static/images gs://my-staging-bucket/build/static/images
+gsutil -m rsync -r ./deployable/build/static/js gs://my-staging-bucket/build/static/js
+
+# set metadata for static objects hosted on cloud storage
+gsutil -m setmeta -h "Content-Encoding: gzip" gs://my-staging-bucket/build/static/css/* \
+                     gs://my-staging-bucket/build/static/js/* \
+                     gs://my-staging-bucket/build/static/images/*
+gsutil -m setmeta -h "Cache-Control: public, max-age=31536000" gs://my-staging-bucket/build/static/css/* \
+                  gs://my-staging-bucket/build/static/js/* \
+                  gs://my-staging-bucket/build/static/images/*
+
+gcloud app deploy deployable/app.yaml -q --version="fe-staging" --no-promote
